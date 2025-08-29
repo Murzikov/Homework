80237fb56ba41c4ea6d63c14eb772bd207c3b051
Saveliy <76556572+Murzikov@users.noreply.github.com>
2025-08-29 17:43:37 +0500

    Add files via upload

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -162,4 +162,11 @@
 
     });
 
+
+    // маска для телефона//
+
+    const telInputs = document.querySelectorAll('input[type="tel"]')
+    const im = new Inputmask('+7 (999) 999-99-99')
+    im.mask(telInputs)
+
 })()
