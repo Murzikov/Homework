4c0594e80c72dd5e5fb38c8b3c2b26ced4bf1d1e
Saveliy <76556572+Murzikov@users.noreply.github.com>
2025-11-24 18:48:23 +0500

    Add files via upload

---
+++ homework10/main-10.js
@@ -0,0 +1,26 @@
+// Задача 1.
+// Создайте переменные с корректными именами, чтобы сохранить следующую информацию:
+
+const firstName = "Савелий"
+const lastName = "Метельков"
+const isStudent = true
+
+// Задача 2.
+const age = 19
+const currentYear = 2025
+const birthYear = (currentYear - age)
+console.log (`да я ${birthYear} года`)
+
+// Задача 3.
+// Выведите в консоли сообщение в таком формате:
+console.log (`я ${firstName}, фамилия ${lastName}, мне ${age} лет. Я ученик курса: ${isStudent}.`)
+
+// Задача 4.
+// Какое значение будет у переменной result?
+// let a = '123';
+// let b = +'456';
+// let c = Number('789');
+// let d = Boolean(0);
+// let e = Boolean(' ');
+// let result = a + b + c + d + e;
+console.log('"123456789falsetrue"?')
